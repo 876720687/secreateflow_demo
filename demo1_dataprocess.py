fb60a40bdc7b1901406a77cc63bec75a2daf1425
876720687 <876720687@qq.com>
2022-11-02 17:55:11 +0800

    demo2

--- demo1_dataprocess.py
+++ demo1_dataprocess.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*- 
-# @Time : 2022/11/1 13:28 
+# @Time : 2022/11/2 14:53 
 # @Author : YeMeng 
-# @File : demo2_spu.py 
+# @File : demo1_dataprocess.py 
 # @contact: 876720687@qq.com
---
+++ demo_spu_logis/main_process.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*- 
+# @Time : 2022/11/2 15:51 
+# @Author : YeMeng 
+# @File : main_process.py 
+# @contact: 876720687@qq.com
+from demo_spu_logis import *
+
+# %matplotlib inline
+
+# Load the data
+from demo_spu_logis.demo2_spu import *
+
+x1, y = breast_cancer(party_id=1, train=True)
+x2, y = breast_cancer(party_id=2, train=True)
+
+# Hyperparameter
+W = jnp.zeros((30,))
+b = 0.0
+epochs = 10
+learning_rate = 1e-2
+
+# Train the model
+losses, W, b = fit(W, b, x1, x2, y, epochs=10, learning_rate=1e-2)
+
+# Plot the loss
+plot_losses(losses)
+
+# Validate the model
+X_test, y_test = breast_cancer(train=False)
+auc=validate_model(W,b, X_test, y_test)
+print(f'auc={auc}')
